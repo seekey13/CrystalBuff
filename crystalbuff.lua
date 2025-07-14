bb1fafc0073b83ffdea6109365ea89d01ab8ea25
seekey13 <97695301+seekey13@users.noreply.github.com>
2025-07-14 13:11:32 -1000

    Create crystalbuff.lua

---
+++ crystalbuff.lua
@@ -0,0 +1,5 @@
+addon.name      = 'CrystalBuff';
+addon.author    = 'Seekey';
+addon.version   = '0.1';
+addon.desc      = 'Automaticaly set Crystal Buff based on zone';
+addon.link      = '[https://github.com/seekey13/CrystalBuff]';
