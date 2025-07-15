b89be44c43989b5381489f53c92e6707362f4c05
seekey13 <97695301+seekey13@users.noreply.github.com>
2025-07-15 12:36:31 -1000

    Update crystalbuff.lua

--- crystalbuff.lua
+++ crystalbuff.lua
@@ -9,7 +9,7 @@ This addon is designed for Ashita v4 and the CatsEyeXI private server.
 
 addon.name      = 'CrystalBuff';
 addon.author    = 'Seekey';
-addon.version   = '1.0';
+addon.version   = '1.1';
 addon.desc      = 'Tracks and corrects crystal buff (Signet, Sanction, Sigil) based on current zone.';
 addon.link      = 'https://github.com/seekey13/CrystalBuff';
 
